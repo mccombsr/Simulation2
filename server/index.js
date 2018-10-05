1ce5808027830d7b5f1dec42adb8584003cf3dfa
Ryan McCombs <mccombs008@yahoo.com>
2018-10-05 10:36:56 -0600

    security test

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const massive = require('massive');
 const app = express();
 
 app.use(bodyParser.json());
 
-const port = 4000;
+const port = process.env.PORT;
 app.listen(port, ()=>{
     console.log(`Port ${port} is here for all your wants and needs.`)});
