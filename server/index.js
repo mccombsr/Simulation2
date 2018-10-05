86ad64b0cd1b849fccbeafcea6f4f24307f74193
Ryan McCombs <mccombs008@yahoo.com>
2018-10-05 10:31:28 -0600

    server setup complete

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json());
+
+const port = 4000;
+app.listen(port, ()=>{
+    console.log(`Port ${port} is here for all your wants and needs.`)});
