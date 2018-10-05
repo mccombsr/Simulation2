995214359e4fbf855a459c2686cb448dd0a4d270
Ryan McCombs <mccombs008@yahoo.com>
2018-10-05 10:55:35 -0600

    database setup complete

--- server/index.js
+++ server/index.js
@@ -2,9 +2,18 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const massive = require('massive');
-const app = express();
 
+const app = express();
 app.use(bodyParser.json());
+
+massive(process.env.CONNECTION_STRING)
+.then((dbInstance)=>{
+    app.set('db', dbInstance);
+    console.log(`DB in the house!`)
+})
+.catch((err)=>{
+    console.log(err)
+})
 
 const port = process.env.PORT;
 app.listen(port, ()=>{
